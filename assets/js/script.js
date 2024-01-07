8b71fa610276627aa43bc3a5aa237fe356b27ec9
CS89 <chrismacaroline@gmail.com>
2024-01-07 00:22:04 +0000

    planner color coding working - set up according to current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,35 @@ displayTime()
 setInterval (displayTime, 1000)
 
 console.log("ding")
+
+function updateHourblocks (){
+  currentHour = dayjs().hour()
+  console.log(currentHour)
+
+  // const plannerHour =$(".time-block")
+  // console.log(plannerHour)
+   const timeBlock =  $(".time-block")
+  $(timeBlock).each(function(){
+
+    let plannerHour = parseInt($(this).attr("id"))
+    console.log(plannerHour)
+  //  console.log(timeBlock)
+    if (plannerHour < currentHour){
+      $(this).addClass("past")
+      $(this).removeClass("present future")
+    
+    }
+    else if (plannerHour === currentHour){
+      $(this).addClass("present")
+      $(this).removeClass("future past")
+    }
+    else if ( plannerHour > currentHour){
+      $(this).addClass("future")
+      $(this).removeClass("past present")
+
+    }
+
+
+
+  })}
+updateHourblocks()
