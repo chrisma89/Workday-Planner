ac5bc966ecef4e7c043f5c78426f65bec1aaf2a9
CS89 <chrismacaroline@gmail.com>
2024-01-07 22:02:24 +0000

    retrieval of saved data and loading onto page- working

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,93 +13,77 @@ displayTime()
 setInterval (displayTime, 1000)
 
 // Color-coded time block based on past, present, and future 
+
 function updateHourblocks (){
   const currentHour = parseInt (dayjs().hour())
-  console.log(currentHour)
-
-  
-   const timeBlock =  $(".time-block")
-  timeBlock.each(function(){
-
-    let plannerHour = parseInt($(this).attr("id"))
-    console.log(plannerHour)
- 
+  const timeBlock =  $(".time-block")
     
-    if (plannerHour === currentHour){
+    timeBlock.each(function(){
+    let plannerHour = parseInt($(this).attr("id"))
+    
+    if (plannerHour === currentHour)
+    {
       $(this).addClass("present")
       $(this).removeClass("future past")
     }
-    else if ( plannerHour < currentHour){
+    else if ( plannerHour < currentHour)
+    {
       $(this).addClass("past")
       $(this).removeClass("future present")
-
     }
-    else if(plannerHour > currentHour){
+    else if(plannerHour > currentHour)
+    {
       $(this).addClass("future")
       $(this).removeClass("past present")
-    
     }
-  })}
+  }
+  )
+}
 
-  updateHourblocks()
-setInterval(updateHourblocks, 3600000)
+// call function upon loading of page
+updateHourblocks()
+
 
 
 // update time-blocks every hour
 setInterval(updateHourblocks, 3600000)
 
+
 // Allow a user to enter an event when they click a time block
+
 const saveButton = $(".btn")
-const textArea = $(".textarea")
-const timeBlockText = $(".time-block")
-const eventDescription = $(".description").val()
 
 saveButton.on("click", function(event){
   event.preventDefault()
   
-
+    // iterate over each time block and store key and value in local storage
     $(".time-block").each( function(){
       let key = parseInt($(this).attr("id"))
       let description = $(this).find(".description").val()
      localStorage.setItem(key , description)
+  console.log((description))
+  
       }) 
-    })
+    });
 
 
-
-// function saveUserEvent () {
-
+// retrieve stored data onto the planner
+function retrieveData(){
   
-//   let UserInput = $(".description").val()
-//   let id= $("#user-input")
-//   // console.log(UserInput)
-//   localStorage.setItem("user-input", UserInput[i])
+  // iterate over each time block to retrieve each block's saved user event
+  $(".time-block").each(function(){
+    let key = parseInt($(this).attr("id"))
+    let description = localStorage.getItem(key)
+    if (description){
+      $(this).find(".description").val(description)
+    }
+  }
+)}
 
-  
-
-//   // let savedInput = localStorage.getItem("user-input")
-//   // UserInput.textContent = savedInput
-//   }
-//   // retrieveData()
-// function saveUserEvent (){
-// $(".description").each(function(i){
-//   let userId = "user-input-" +i
-// let UserInput = $(this).val()})
-// localStorage.setItem(userId, UserInput)
-// }
+// call function to retrieve data 
+retrieveData()
 
 
 
-// function retrieveData (UserInput){
-//   let savedInput = localStorage.getItem(UserInput)
-
-//   if(UserInput) {
-//     UserInput.text(savedInput)
-//   }
-  
-// }
-// $(function(){
-//   retrieveData()
-// })
 
 
